979657c11f279f160f968b579fcdf65421f83f1f
ratulsaqibkhan <ratulkhan.jhenidah@gmail.com>
2018-12-16 00:56:59 +0600

    Leap Year test

---
+++ leap_year_test.py
@@ -0,0 +1,9 @@
+year = input("Type a year and press enter ")
+year = int(year)
+if(year % 100 != 0 and year % 4 == 0):
+    print(year,'is a leap year')
+elif(year % 100 == 0 and year % 4 == 0):
+    print(year,'is a leap year')
+else:
+    print(year,'is not a leap year')
+
